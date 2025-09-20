da3afa83ff9e8bbe1c13bf9b436041e020e16ba7
ismailerguldev <ismailergul.dev@gmail.com>
2025-09-21 02:33:03 +0300

    Project Template Created

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { DarkTheme, DefaultTheme } from '@react-navigation/native';
+import { createURL } from 'expo-linking';
+import * as SplashScreen from 'expo-splash-screen';
+import * as React from 'react';
+import { useColorScheme } from 'react-native';
+import { Navigation } from './navigation';
+
+SplashScreen.preventAutoHideAsync();
+
+const prefix = createURL('/');
+
+export function App() {
+  const colorScheme = useColorScheme();
+
+  const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme
+
+  return (
+    <Navigation
+      theme={theme}
+      linking={{
+        enabled: 'auto',
+        prefixes: [prefix],
+      }}
+      onReady={() => {
+        SplashScreen.hideAsync();
+      }}
+    />
+  );
+}
