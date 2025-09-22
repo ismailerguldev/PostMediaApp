1ffa337d97a1ff7532c64c92e8c25dc0b1d0f3e2
ismailerguldev <ismailergul.dev@gmail.com>
2025-09-22 18:06:50 +0300

    HomePage No Post Widget

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -5,8 +5,11 @@ import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { useEffect } from 'react';
 import { getLikeds } from '../../services/PostCacheService';
 import { getPosts } from '../../services/PostService';
+import { Icons } from '../../assets/icons/icons';
+import { useTheme } from '@react-navigation/native';
 export const Home: React.FC = () => {
   const isLoading = useAppSelector((state) => state.user.isFetchingPosts)
+  const { colors } = useTheme()
   const dispatch = useAppDispatch()
   useEffect(() => {
     (async () => { await dispatch(getLikeds()); })()
@@ -17,7 +20,10 @@ export const Home: React.FC = () => {
       {
         isLoading === "fulfilled" ?
           <PostsSection />
-          : isLoading === "pending" ? <ActivityIndicator size={"large"} color={"black"} /> : <Text>hi</Text>
+          : isLoading === "pending" ? <ActivityIndicator size={"large"} color={"black"} /> : <VBox style={{ justifyContent: "center", alignItems: "center", gap: 16 }}>
+            <Icons.Sad color={colors.text} style={{ opacity: 0.5 }} size={48} />
+            <Text style={{ fontSize: 16, color: colors.text, opacity: 0.5 }}>There is no posts here...</Text>
+          </VBox>
       }
     </VBox>
   );
